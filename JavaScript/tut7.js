edce33aa1eae68d993f141eb3dcad5bc09fdcc25
juveria dalvi <juveriadalvi4@gmail.com>
2021-10-30 18:19:45 +0530

    practicing conditional statements

---
+++ JavaScript/tut7.js
@@ -0,0 +1,23 @@
+//? VIDEO_7 - IF/ELSE CONDITIONALS & SWITCHES IN JAVASCRIPT
+console.log(`Tutorial-7`);
+
+// const programmingLang = `JavaScript`;
+// if (programmingLang == `PHP`) {
+// 	console.log(`I'm learning PHP`);
+// } else {
+// 	console.log(`I'm Learning JavaScript`);
+// }
+
+let lang = `Python`;
+if (lang == `PHP`)
+ {
+	console.log(`I'm learning PHP`);
+} 
+else if (lang == `Python`); 
+{
+	console.log(`I'm Learning Python`);
+}
+else 
+{
+	console.log(`I'm Learning JavaScript`);
+}
