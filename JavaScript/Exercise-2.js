f7ddde34260c0c880c5f18f2a98a472493c4ace1
DJ <85214519+JuveriaD@users.noreply.github.com>
2021-11-07 21:46:38 +0530

    Update Exercise-2.js

--- JavaScript/Exercise-2.js
+++ JavaScript/Exercise-2.js
@@ -1,5 +1,5 @@
 //? Use prompt box with Number() function and create two const variables 
 //? concat the two numbers entered in prompt in the console.log
-// const userNumber1 = Number( prompt('please enter any number?'));
-// const userNumber2 = Number( prompt('please enter another Number to add?'));
-// console.log(userNumber1 + userNumber2);
+const userNumber1 = Number( prompt('please enter any number?'));
+const userNumber2 = Number( prompt('please enter another Number to add?'));
+console.log(userNumber1 + userNumber2);
