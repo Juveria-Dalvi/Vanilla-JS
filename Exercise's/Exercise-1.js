01e69b929a58351edb360edfc8be33f0d944fec0
juveria dalvi <juveriadalvi4@gmail.com>
2021-11-08 09:35:58 +0530

    changed files settings

--- Exercise's/Exercise-1.js
+++ Exercise's/Exercise-1.js
@@ -1,19 +1,19 @@
-console.log("Exercise-1");
-
-//! => capitilize a string example (Awesome) as u can see only first letter capital rest of them are in lowercase 
+console.log('Exercise-1');
+// ********************** Exercise 1 STARTS HERE **********************
+//! => capitilize a string example (Awesome) as u can see only first letter capital rest of them are in lowercase
 
 //* steps u need to follow
 
-//? 1.. 🚀  create a constant string variable in lowercase letters 
+//? 1.. 🚀  create a constant string variable in lowercase letters
 
 //? 2.. 🚀  use charAt method to return character from string
 
 //? 3.. 🚀  use upperCaseMethod to convert return string from charAt
 
-//? 4.. 🚀  use slice or substring method then use concatenation method to combine & output the result 
-
+//? 4.. 🚀  use slice or substring method then use concatenation method to combine & output the result
+// ********************** ANSWER BELOW **********************
 //!created string in lowercase
-const myVar1="ball";
+const myVar1 = 'ball';
 let myVar2;
 let myVar3;
 let output;
@@ -21,24 +21,23 @@ let output;
 // myVar2=myVar1.replace('ball', 'Call');
 
 //!charAt method to return character
-myVar2=myVar1.charAt(0);
+myVar2 = myVar1.charAt(0);
 console.log(myVar2);
 
 // ! upperCaseMethod
-myVar2=myVar2.toUpperCase();
+myVar2 = myVar2.toUpperCase();
 console.log(myVar2);
 
 //!slice or substring method
-myVar3=myVar1.substring(1)
+myVar3 = myVar1.substring(1);
 console.log(myVar3);
 
 //!concatenation method
-output=myVar2.concat(myVar3)
+output = myVar2.concat(myVar3);
 console.log(output);
 
 //! only create one variable const str = 'learning'
 //! n output the result in one line n koii variable create nii krna ok ic str k ilawa
 
-const str ='learning';
+const str = 'learning';
 console.log(str.charAt(0).toUpperCase() + str.substring(1));
-
---
+++ Exercise's/Exercise-2.js
@@ -0,0 +1,8 @@
+// ********************** Exercise 2 STARTS HERE **********************
+//? create two const variables
+//? Use prompt box and convert string into Number() function
+//? concat the two numbers entered in prompt in your console so that you get the Addition of entered number
+// ********************** ANSWER BELOW **********************
+const userNumber1 = Number(prompt('please enter any number?'));
+const userNumber2 = Number(prompt('please enter another Number to add?'));
+console.log(userNumber1 + userNumber2);
