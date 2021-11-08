ff3da1fe33eee21b5edf4e9cf968897086d1f2a0
DJ <85214519+JuveriaD@users.noreply.github.com>
2021-11-08 21:55:18 +0530

    for loops

---
+++ Exercise's/Exercise 4.js
@@ -0,0 +1,29 @@
+// ********************** Exercise 4 STARTS HERE **********************
+// Get the sum of two arrays...actually the sum of all their elements. 
+// P.S. Each array includes only integer numbers. 
+// Example input arrays:
+let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
+let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
+//? 🚀 best of luck 
+//* *************************ANSWER BELOW*****************************
+
+// console.log(3+5+22+5+7+2+45+75+89+21+2);
+// console.log(9+2+42+55+71+22+4+5+90+25+26);
+
+let total=0;
+for (let i=0;i<arr_1.length;i++)
+  {
+  // console.log(arr_1[i]);
+    total+=arr_1[i]
+    // console.log(Number(total))
+  }
+
+
+let total2=0;
+for (let i=0;i<arr_2.length;i++)
+  {
+    total2+=arr_2[i]
+  }
+
+
+console.log('I still need to work on Logic i got the answer bychance')
