1177a65018093f7d086d5beafbef74799907eac4
DJ <85214519+JuveriaD@users.noreply.github.com>
2021-11-11 20:58:35 +0530

    Update Exercise-2.js

--- Exercise's/Exercise-2.js
+++ Exercise's/Exercise-2.js
@@ -6,3 +6,16 @@
 const userNumber1 = Number(prompt('please enter any number?'));
 const userNumber2 = Number(prompt('please enter another Number to add?'));
 console.log(userNumber1 + userNumber2);
+
+// ********************** Exercise 2 STARTS HERE **********************
+//? create const variables
+//? Use any box of your choice and ask user to input Age
+//? Show Age should be greater then 18 to join the club
+// ********************** ANSWER BELOW **********************
+
+const USER = Number(prompt ("What is your age?"));
+if(USER >= 18){
+console.log("You can join our club");
+}else{
+console.log("You can't join our club");
+}
